8fa1b9778c4fec115ea50f333307634a1fd7139c
kazuya kawaguchi <kawakazu80@gmail.com>
2019-01-15 14:04:03 +0900

    :shirt: refactor: indent

--- generator/templates/docs/docs/_vuepress/config.js
+++ generator/templates/docs/docs/_vuepress/config.js
@@ -1,28 +1,28 @@
 module.exports = {
   locales: {
-  	'/': {
-  	  lang: 'en-US',
-  	  title: '<%= projectName %>',
-  	  description: '<%= projectName %> for Vue.js'
-  	}
+    '/': {
+      lang: 'en-US',
+      title: '<%= projectName %>',
+      description: '<%= projectName %> for Vue.js'
+    }
   },
   themeConfig: {
-  	repo: '<%= author %>/<%= repoName %>',
-  	docsDir: 'docs',
-  	locales: {
-  	  '/': {
-  	  	label: 'English',
-  	  	selectText: 'Languages',
-  	  	editLinkText: 'Edit this page on GitHub',
-  	  	nav: [{
-  	  	  text: 'Release Notes',
-  	  	  link: 'https://github.com/<%= author %>/<%= repoName %>/releases'
-  	  	}],
-  	  	sidebar: [
-  	  	  '/installation.md',
-  	  	  '/started.md',
-  	  	]
-  	  }
-  	}
+    repo: '<%= author %>/<%= repoName %>',
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        nav: [{
+          text: 'Release Notes',
+          link: 'https://github.com/<%= author %>/<%= repoName %>/releases'
+        }],
+        sidebar: [
+          '/installation.md',
+          '/started.md',
+        ]
+      }
+    }
   }
 }
