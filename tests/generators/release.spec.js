6620b3eede018457d0eb8ef8af96f3c27d6f6dd1
kazuya kawaguchi <kawakazu80@gmail.com>
2019-01-26 10:05:42 +0900

    :shirt: refactor: add console warn suppress mocking at generators

--- tests/generators/release.spec.js
+++ tests/generators/release.spec.js
@@ -1,5 +1,20 @@
-jest.mock('/package.json', () => () => {}, { virtual: true })
 const generateWithPlugin = require('@vue/cli-test-utils/generateWithPlugin')
+
+let spy
+beforeEach(() => {
+  const utils = require('../../lib/utils')
+  spy = jest.spyOn(utils, 'loadPackage')
+  spy.mockImplementation(api => ({
+    name: 'vue-i18n',
+    author: 'kazuya kawaguchi',
+    version: '0.0.1',
+    license: 'MIT'
+  }))
+})
+
+afterEach(() => {
+  spy.mockClear()
+})
 
 test('release', async () => {
   const projectName = 'vue-i18n-gen-release'
