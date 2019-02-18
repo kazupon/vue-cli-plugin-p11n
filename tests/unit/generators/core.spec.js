0322146344344a50532dd122ce8b1e19762fa4a4
EvanBurbidge <burbidge@tcd.ie>
2019-02-18 16:51:26 +0000

    :zap: improvement(core): support scoped package #10 (#12) by @EvanBurbidge
    * added stripNamespaceIfExists, updated tests, updated generator/index
    
    * clean up

--- tests/unit/generators/core.spec.js
+++ tests/unit/generators/core.spec.js
@@ -27,22 +27,7 @@ test('javascript', async () => {
     apply: require('../../../generator'),
     options: {}
   }])
-
-  // check pkg
-  expect(pkg.sideeffects).toBe(false)
-  expect(pkg.main).toBe(`dist/${projectName}.common.js`)
-  expect(pkg.jsdelivr).toBe(`dist/${projectName}.umd.min.js`)
-  expect(pkg.module).toBe(`dist/${projectName}.esm.js`)
-  expect(pkg.unpkg).toBe(`dist/${projectName}.umd.min.js`)
-  const distFiles = [
-    `dist/${projectName}.common.js`,
-    `dist/${projectName}.umd.min.js`,
-    `dist/${projectName}.umd.js`,
-    `dist/${projectName}.esm.js`,
-    'src'
-  ]
-  distFiles.forEach(file => { expect(pkg.files).toContain(file) })
-
+  checkPackageExpectations(pkg, projectName);
   // check files
   const plugin = files['src/index.js']
   expect(plugin).toMatch(`Vue.prototype.$add = (a, b) => {`)
@@ -73,3 +58,41 @@ test('typescript', async () => {
   expect(plugin).toMatch(`import { VueConstructor, PluginObject } from 'vue'`)
   expect(dts).toMatch(`type PluginAddFunction = (a: number, b: number) => number`)
 })
+
+test('javascript with namespace', async () => {
+  const projectName = '@testing/vue-i18n-gen-js';
+  const projectNameNoNamespace = 'vue-i18n-gen-js';
+
+  const { pkg, files } = await generateWithPlugin([{
+    id: '@vue/cli-service',
+    apply: () => {},
+    options: { projectName }
+  }, {
+    id: 'p11n',
+    apply: require('../../../generator'),
+    options: {}
+  }])
+  checkPackageExpectations(pkg, projectNameNoNamespace);
+  // check files
+  const plugin = files['src/index.js']
+  expect(plugin).toMatch(`Vue.prototype.$add = (a, b) => {`)
+})
+
+function checkPackageExpectations (pkg, name) {
+  // check pkg
+  console.log('checking package');
+  expect(pkg.sideeffects).toBe(false)
+  expect(pkg.main).toBe(`dist/${name}.common.js`)
+  expect(pkg.jsdelivr).toBe(`dist/${name}.umd.min.js`)
+  expect(pkg.module).toBe(`dist/${name}.esm.js`)
+  expect(pkg.unpkg).toBe(`dist/${name}.umd.min.js`)
+  const distFiles = [
+    `dist/${name}.common.js`,
+    `dist/${name}.umd.min.js`,
+    `dist/${name}.umd.js`,
+    `dist/${name}.esm.js`,
+    'src'
+  ];
+
+  distFiles.forEach(file => { expect(pkg.files).toContain(file) })
+}
